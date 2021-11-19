f305825e130f97116070b6dcc0b5ae7faf966b0e
abulhashim <abulhashim351@gmail.com>
2021-11-19 20:54:26 +0500

    Initial Commit

---
+++ HiDoctor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiDoctor
+//
+//  Created by Ameen Ahmed on 19/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
